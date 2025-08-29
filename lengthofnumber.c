ae280b65eee668394b3b177b9ae9c8227454b512
RPSK <78362587+RPSK@users.noreply.github.com>
2025-08-29 20:52:09 +0530

    Create lengthofnumber.c

---
+++ lengthofnumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main(){
+	int user_number;
+	printf("input your number here:");
+	scanf("%d",&user_number);
+	 int i;
+	for(i=0;user_number!=0;i++){
+	    
+		user_number=user_number/10;
+	}
+	printf("%d",i);
+}
